e13628a9185a2c3489e1e406a00b3ba75df43e28
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2024-09-13 18:26:37 -0300

    chore: eureka routing options

--- src/main/java/github/samuelastech/payment/Payment.java
+++ src/main/java/github/samuelastech/payment/Payment.java
@@ -9,7 +9,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.context.annotation.Primary;
 
 import java.math.BigDecimal;
 
